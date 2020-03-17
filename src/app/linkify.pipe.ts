13d362c8a974b35b2e2efd5c98443fb4e306f3ae
Deven Swiergiel <Deven.Swiergiel@its.ny.gov>
2020-03-17 10:45:46 -0400

    Detects links and wraps them with a tag to be clickable

--- src/app/linkify.pipe.ts
+++ src/app/linkify.pipe.ts
@@ -1,31 +1,42 @@
-import { Pipe, PipeTransform,SecurityContext  } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { Pipe, PipeTransform, SecurityContext } from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Pipe({
-  name: 'linkify'
+  name: "linkify"
 })
 export class LinkifyPipe implements PipeTransform {
-
   constructor(private _domSanitizer: DomSanitizer) {}
 
   transform(value: any, args?: any): any {
     //return this._domSanitizer.bypassSecurityTrustHtml(this.stylize(value));
 
-    return  this._domSanitizer.sanitize(SecurityContext.HTML, this._domSanitizer.bypassSecurityTrustHtml(this.stylize(value)))
+    return this._domSanitizer.sanitize(
+      SecurityContext.HTML,
+      this._domSanitizer.bypassSecurityTrustHtml(this.urlify(value))
+    );
+
+    // return this._domSanitizer.sanitize(
+    //   SecurityContext.HTML,
+    //   this._domSanitizer.bypassSecurityTrustHtml(this.stylize(value))
+    // );
   }
 
   private stylize(text: string): string {
-    let stylizedText: string = '';
+    let stylizedText: string = "";
     if (text && text.length > 0) {
       for (let t of text.split(" ")) {
-        if (t.startsWith("@") && t.length>1)
+        if (t.startsWith("@") && t.length > 1)
           stylizedText += `<a href="#${t.substring(1)}">${t}</a> `;
-        else
-          stylizedText += t + " ";
+        else stylizedText += t + " ";
       }
       return stylizedText;
-    }
-    else return text;
+    } else return text;
   }
 
+  private urlify(text: string): string {
+    var urlRegex = /(https?:\/\/[^\s]+)/g;
+    return text.replace(urlRegex, function(url) {
+      return '<a target="_blank" href="' + url + '">' + url + "</a>";
+    });
+  }
 }
